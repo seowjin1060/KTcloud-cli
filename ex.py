491195d3702b1fde6a9789fc9413067a4f605733
SEO WON JIN <seowjin1060@naver.com>
2019-02-19 17:08:48 +0900

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,13 @@
+import sys
+def configure():
+  id = input("enter ID")
+  secrete = input("endter PW")
+  
+
+
+print("============")
+for i,value in enumerate(sys.argv):
+  print("command: "%(value))
+  if command == "configure":
+    configure()
+  elif comman == "configure list":
