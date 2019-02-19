8e6c7410460ffcb5fdbe70a67309ca64eb7cde99
SEO WON JIN <seowjin1060@naver.com>
2019-02-19 17:08:27 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup_requires = []
+
+
+install_requires = []
+
+dependency_links = []
+setup(
+    name = 'ucloudcli',
+    version = '0.1',
+    description = 'ucloud command line interface',
+    author = 'WonJinSeo',
+    author_email = 'seowjin1060@naver.com',
+    packages = ["uCloud"],
+    include_package_data = True,
+    install_requires=install_requires,
+    setup_requires = setup_requires,
+    dependency_links=dependency_links,
+    #scripts=['manage.py'],
+    entry_points={
+        'console_scripts': [],
+        'egg_info.writers':[],
+        }
+    
+
+    )
