604d711e5e1d5d54abe4222fc82a22e502338dc4
SEO WON JIN <seowjin1060@naver.com>
2019-02-21 14:59:00 +0900

    Add files via upload

--- credit_configure.py
+++ credit_configure.py
@@ -6,6 +6,22 @@ class configure:
         self.commands = ["init","list"]
         self.ZONE= {"KOR-Central A":"1", "KOR-Central B":'2', "KOR-HA":'3', "KOR-Seoul M2":"4", "JPN":"5", "US-West":"6",}
         self.RESPONSE_TYPE=['json','xml']
+    def configure_list(self):
+        try:
+            credit_file = open("credit.txt","r")
+        except:
+            print("no credit file detected \nplease type 'ucloud configure init' to create credit files")
+            exit(-1)
+        credit =[]
+        for line in credit_file:
+            credit.append(line)
+        if(len(credit) <3):
+            print("credit file is not completed \n type 'ucloudcli configure init' to complete credit first")
+        print("API_KEY : ", credit[0])
+        print("Secret Key : ", credit[1])
+        print("Zone : ", credit[2])
+        print("response Type: ", credit[3])
+        return
     def configure_init(self):
         m2_zone = False
         apikey_in = input("[api_key] :")
@@ -23,6 +39,7 @@ class configure:
             m2_zone = True
         t = open("credit.txt","w")
         t.write(apikey_in+"\n")
+        #if(secret_in != ""):
         t.write(secret_in+"\n")
         t.write(zone_in+"\n")
         t.write(response+"\n")
@@ -30,16 +47,7 @@ class configure:
         t.close()
         return
 
-    def configure_list(self):
-        credit_file = open("credit.txt" , "r")
-        credit =[]
-        for line in credit_file:
-            credit.append(line)
-        print("API_KEY : ", credit[0])
-        print("Secret Key : ", credit[1])
-        print("Zone : ", credit[2])
-        print("response Type: ", credit[3])
-        return
+
     
     def command_process_configure(self):
         command = self.command
