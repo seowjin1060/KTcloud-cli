e4774539fb983b412d3378d77b73f13096cf08eb
seowjin1060 <seowjin1060@naver.com>
2019-03-14 16:55:31 +0900

    enter

--- setup.py
+++ setup.py
@@ -10,19 +10,17 @@ setup(
     version = '0.1',
     description = 'ucloud command line interface',
     author = 'WonJinSeo',
-    author_email = 'seowjin1060@naver.com',
-    packages = find_packages(exclude = ("credit.txt",)),
+    author_email = 'seowjin1060@gmail.com',
+    packages = find_packages(exclude = ("credit.txt",".DS")),
     include_package_data = True,
     install_requires=install_requires,
     setup_requires = setup_requires,
     dependency_links=dependency_links,
     license="Apache License 2.0",
-   # scripts=['bin/command'],
+    scripts=['bin/bash_setup'],
     #scripts=['manage.py'],
     entry_points={
         'console_scripts': [],
         'egg_info.writers':[],
         }
-    
-
     )
